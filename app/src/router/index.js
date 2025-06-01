a9e104d85e0afff8c53a51631a58cbd9223a21cc
BelutijNazar <nazar16312@mail.ru>
2025-06-01 22:59:00 +0300

    Configure to deploy

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,5 @@
 // router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import RegistrationPage from '../components/RegistrationPage.vue'; 
 import LoginPage from '../components/LoginPage.vue'; 
 import ChatPage from '../components/ChatPage.vue'; 
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
